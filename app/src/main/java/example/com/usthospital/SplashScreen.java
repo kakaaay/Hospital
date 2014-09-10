b9e5a62120ffc16ed795a06cf6b6b21fc268b654
kakaaay <2012048055@ust-ics.mygbiz.com>
2014-09-10 21:55:56 +0800

    UstHospital
    This is to provide online appointment service to UST hospital patients

---
+++ app/src/main/java/example/com/usthospital/SplashScreen.java
@@ -0,0 +1,44 @@
+package example.com.usthospital;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import example.com.usthospital.R;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.Window;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(SplashScreen.this,Home.class);
+                startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash_screen, menu);
+        return true;
+    }
+
+}
